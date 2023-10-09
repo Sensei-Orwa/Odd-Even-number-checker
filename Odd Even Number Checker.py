c06ba5da18727f29201c9a1f55a4f246b9eabb45
Sensei Orwa <146319656+Sensei-Orwa@users.noreply.github.com>
2023-10-09 22:52:44 +0300

    Add files via upload

---
+++ Odd Even Number Checker.py
@@ -0,0 +1,18 @@
+#Write a function that takes an integer as an argument and prints whether it's even or odd.
+
+def odd_checker(n):
+    if not n % 2 == 0:
+        print(f"{n} is an odd number")
+    elif n % 2 == 0:
+        print(f"{n} is an even number.")
+    else:
+        print("invalid input")
+
+o
+
+
+
+
+
+
+
